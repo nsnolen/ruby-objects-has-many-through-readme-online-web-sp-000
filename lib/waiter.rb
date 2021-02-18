08107513044dfd638ea4c78fd41c4feba28ce21e
Sara Nolen <nolensara1@gmail.com>
2021-02-18 00:49:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,7 +21,7 @@ class Waiter
   end
 
   def best_tipper
-    best_tipped_meal meal.max{|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+    best_tipped_meal = meal.max{|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
     best_tipped_meal
   end
 end
